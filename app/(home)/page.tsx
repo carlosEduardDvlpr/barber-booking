22881607b47181d8d7e4eb0e397026a2f2e6c03f
carlos <carlos8189.mk@gmail.com>
2024-10-07 10:20:02 -0300

    fix: rename a component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -6,7 +6,7 @@ import { BookingItem } from '../_components/booking-item';
 import { db } from '../_lib/prisma';
 import { BarbershopItem } from './_components/barbershop-item';
 
-export default async function Home() {
+export default async function HomePage() {
   const barbershop = await db.barbershop.findMany({});
 
   return (
