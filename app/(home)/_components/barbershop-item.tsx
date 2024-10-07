40212139e34b55d1730bb70f6c90ff4a143df3e1
carlos <carlos8189.mk@gmail.com>
2024-10-07 10:20:23 -0300

    feat: add ServiceItem component

--- app/(home)/_components/barbershop-item.tsx
+++ app/(home)/_components/barbershop-item.tsx
@@ -1,19 +1,29 @@
+'use client';
 import { Badge } from '@/app/_components/ui/badge';
 import { Button } from '@/app/_components/ui/button';
 import { Card, CardContent } from '@/app/_components/ui/card';
 import type { Barbershop } from '@prisma/client';
 import { StarIcon } from 'lucide-react';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 export const BarbershopItem = ({ barbershop }: { barbershop: Barbershop }) => {
+  const router = useRouter();
+  const handleBookingClick = () => {
+    router.push(`/barbershops/${barbershop.id}`);
+  };
+
   return (
     <Card className="min-w-[167px] max-w-[167px] rounded-2xl">
       <CardContent className="px-1 py-0">
         <div className="px-1 w-full h-[159px] relative">
           <div className="absolute top-2 left-2 z-50">
-            <Badge variant="secondary" className="rounded-full opacity-90 flex gap-2 items-center">
-              <StarIcon size={12} className='fill-primary text-primary'/>
-              <span className='text-xs'>5,0</span>
+            <Badge
+              variant="secondary"
+              className="rounded-full opacity-90 flex gap-2 items-center"
+            >
+              <StarIcon size={12} className="fill-primary text-primary" />
+              <span className="text-xs">5,0</span>
             </Badge>
           </div>
 
@@ -38,7 +48,11 @@ export const BarbershopItem = ({ barbershop }: { barbershop: Barbershop }) => {
           <p className="text-sm text-gray-400 overflow-hidden text-ellipsis text-nowrap">
             {barbershop.address}
           </p>
-          <Button className="w-full mt-3" variant="secondary">
+          <Button
+            onClick={handleBookingClick}
+            className="w-full mt-3"
+            variant="secondary"
+          >
             Reservar
           </Button>
         </div>
---
+++ app/barbershops/[id]/page.tsx
@@ -0,0 +1,42 @@
+import { db } from '@/app/_lib/prisma';
+import { BarbershopInfo } from './_components/barbershop-info';
+import { ServiceItem } from './_components/service-item';
+
+interface ParamsProp {
+  params: {
+    id?: string;
+  };
+}
+
+export default async function BarbershopDetailsPage({ params }: ParamsProp) {
+  if (!params.id) {
+    // TODO - redirecionar para home page
+    return null;
+  }
+
+  const barbershop = await db.barbershop.findUnique({
+    where: {
+      id: params.id,
+    },
+    include: {
+      services: true,
+    },
+  });
+
+  if (!barbershop) {
+    // TODO - redirecionar para home page
+    return null;
+  }
+
+  return (
+    <div>
+      <BarbershopInfo barbershop={barbershop} />
+
+      <div className="px-5 flex flex-col gap-4 py-6">
+        {barbershop.services.map((service) => (
+          <ServiceItem key={service.id} service={service} />
+        ))}
+      </div>
+    </div>
+  );
+}
