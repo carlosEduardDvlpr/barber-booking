34b30059fbb7d3d03fed3bd146a7b7575d4cd658
carlos <carlos8189.mk@gmail.com>
2024-10-07 22:08:19 -0300

    feat: ensure unauthenticated user is redirected to login on booking click

--- app/barbershops/[id]/_components/barbershop-info.tsx
+++ app/barbershops/[id]/_components/barbershop-info.tsx
@@ -16,7 +16,7 @@ export const BarbershopInfo = ({ barbershop }: BarbershopInfoProps) => {
   const router = useRouter();
 
   const handleBackClick = () => {
-    router.back();
+    router.replace('/');
   };
 
   return (
